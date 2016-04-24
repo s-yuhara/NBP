c43a14813c2ef9fb60c52c65734bc1275425c01d
S.Yuhara <verlenheit@gmail.com>
2016-04-24 18:17:00 +0900

    Revert "完了 10.1.4の前の状態"
    This reverts commit c35381dc3fc029fa3047151111fff8e015d58c98.

--- Chap10/src/java/beans/UserEntity01.java
+++ Chap10/src/java/beans/UserEntity01.java
@@ -1,34 +1,25 @@
 package beans;
 
 import java.io.Serializable;
-
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name="USER_DATA_5")
+@Table(name="USER_DATA")
 public class UserEntity01 implements Serializable{
-
 @Id @NotNull
 private String id;
-
 private String name;
 private String zipcode;
 private String address;
-
-@Embedded
-private Contact co;
-
 public UserEntity01(){}
-public UserEntity01(String id, String name ,String zipcode,String address,Contact co){
+public UserEntity01(String id, String name ,String zipcode,String address){
     this.id =id;
     this.name = name;
     this.zipcode = zipcode;
     this.address = address;
-    this.co = co;
 }
 
 
@@ -38,14 +29,6 @@ public UserEntity01(String id, String name ,String zipcode,String address,Contac
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getTelephone() {
-        return co.getTelephone();
-    }
-
-    public String getMail() {
-        return co.getMail();
     }
 
     public String getName() {
