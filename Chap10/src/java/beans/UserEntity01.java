c35381dc3fc029fa3047151111fff8e015d58c98
S.Yuhara <verlenheit@gmail.com>
2016-04-24 18:02:39 +0900

    完了 10.1.4の前の状態

--- Chap10/src/java/beans/UserEntity01.java
+++ Chap10/src/java/beans/UserEntity01.java
@@ -1,25 +1,34 @@
 package beans;
 
 import java.io.Serializable;
+
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name="USER_DATA")
+@Table(name="USER_DATA_5")
 public class UserEntity01 implements Serializable{
+
 @Id @NotNull
 private String id;
+
 private String name;
 private String zipcode;
 private String address;
+
+@Embedded
+private Contact co;
+
 public UserEntity01(){}
-public UserEntity01(String id, String name ,String zipcode,String address){
+public UserEntity01(String id, String name ,String zipcode,String address,Contact co){
     this.id =id;
     this.name = name;
     this.zipcode = zipcode;
     this.address = address;
+    this.co = co;
 }
 
 
@@ -29,6 +38,14 @@ public UserEntity01(String id, String name ,String zipcode,String address){
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getTelephone() {
+        return co.getTelephone();
+    }
+
+    public String getMail() {
+        return co.getMail();
     }
 
     public String getName() {
